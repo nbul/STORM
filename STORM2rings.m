5e96ac0daa42ed68fba026cd987a692a12ba405d
nbul <nbul001@gmail.com>
2017-06-30 17:06:10 +0100

    Average
    Average lines and fit added

--- STORM2rings.m
+++ STORM2rings.m
@@ -42,12 +42,14 @@ linedir = [resultdir,'/Lines'];
 
 Index = zeros(numel(files),1);
 Distance = zeros(numel(files),1);
+Line1 = zeros(numel(files),2);
+Line2 = zeros(numel(files),2);
 
 %% Reading files and all coordinates
 for i=1:numel(files)
     Number1 = [num2str(i),'.csv'];
     Ring{i} = csvread(Number1, 1, 1);
-    coordinates{i} = [Ring{i}(:,3) Ring{i}(:,4)];
+    coordinates{i} = [Ring{i}(:,3) Ring{i}(:,4) Ring{i}(:,Column_intensity)];
 end
 
 Sigma = 'full'; % possible to change to 'diagonal'
@@ -55,12 +57,10 @@ nSigma = numel(Sigma);
 SharedCovariance = false; % possible to change to 'true'
 threshold = sqrt(chi2inv(0.99,2));
 options = statset('MaxIter',1000);
-
+k=2;
+d = 500;
 %% Clustering
 for l=1:numel(files)
-    
-    k=2;
-    d = 500;
     x1 = linspace(min(coordinates{l}(:,1)) - 2,max(coordinates{l}(:,1)) + 2,d);
     x2 = linspace(min(coordinates{l}(:,2)) - 2,max(coordinates{l}(:,2)) + 2,d);
     [x1grid,x2grid] = meshgrid(x1,x2);
@@ -68,9 +68,9 @@ for l=1:numel(files)
     i = 0;
     while i == 0;
         image1 = figure;
-        gmfit = fitgmdist(coordinates{l},k,'CovarianceType',Sigma,...
+        gmfit = fitgmdist(coordinates{l}(:,1:2),k,'CovarianceType',Sigma,...
             'SharedCovariance',SharedCovariance,'Options',options);
-        clusterX = cluster(gmfit,coordinates{l});
+        clusterX = cluster(gmfit,coordinates{l}(:,1:2));
         mahalDist = mahal(gmfit,X0);
         coordinates2{l} = [coordinates{l} clusterX];
         h1 = gscatter(coordinates{l}(:,1),coordinates{l}(:,2),clusterX);
@@ -96,8 +96,8 @@ end
 
 %% Identifying a brighter cluster for fit
 for i=1:numel(files)
-    IntensityCluster{i}(1) = sum(Ring{i}(coordinates2{i}(:,3)==1,Column_intensity));
-    IntensityCluster{i}(2) = sum(Ring{i}(coordinates2{i}(:,3)==2,Column_intensity));
+    IntensityCluster{i}(1) = sum(coordinates2{i}(coordinates2{i}(:,4)==1,3));
+    IntensityCluster{i}(2) = sum(coordinates2{i}(coordinates2{i}(:,4)==2,3));
     if IntensityCluster{i}(1)>IntensityCluster{i}(2)
         Index(i) = 1;
     else
@@ -121,27 +121,30 @@ end
 
 %% Fitting clusters with lines and finding distance
 for i=1:numel(files)
-    Line1 = polyfit(coordinates3{i}(coordinates3{i}(:,3)==Index(i),1),coordinates3{i}(coordinates3{i}(:,3)==Index(i),2), 1);
+    Line1(i,:) = polyfit(coordinates3{i}(coordinates3{i}(:,4)==Index(i),1),coordinates3{i}(coordinates3{i}(:,4)==Index(i),2), 1);
     image2 = figure;
-    plot(coordinates2{i}(coordinates2{i}(:,3)==1,1),coordinates2{i}(coordinates2{i}(:,3)==1,2), 'r*');
+    plot(coordinates2{i}(coordinates2{i}(:,4)==1,1),coordinates2{i}(coordinates2{i}(:,4)==1,2), 'r*');
     hold on
-    plot(coordinates2{i}(coordinates2{i}(:,3)==2,1),coordinates2{i}(coordinates2{i}(:,3)==2,2), 'g*');
+    plot(coordinates2{i}(coordinates2{i}(:,4)==2,1),coordinates2{i}(coordinates2{i}(:,4)==2,2), 'g*');
     hold on
-    x1 = min(coordinates2{i}(coordinates2{i}(:,3)==Index(i),1)):0.005:max(coordinates2{i}(coordinates2{i}(:,3)==Index(i),1));
-    y1 = Line1(1,1)*x1 + Line1(1,2);
+    x1 = min(coordinates2{i}(coordinates2{i}(:,4)==Index(i),1)):0.005:max(coordinates2{i}(coordinates2{i}(:,4)==Index(i),1));
+    y1 = Line1(i,1)*x1 + Line1(i,2);
     plot(x1, y1 , '-b', 'LineWidth',3);
     hold on;
     ft = fittype('a*x + b','coefficients','b','independent','x','problem','a');
-    Line2 = fit(coordinates3{i}(coordinates3{i}(:,3)~=Index(i),1),coordinates3{i}(coordinates3{i}(:,3)~=Index(i),2), ft, 'problem',Line1(1,1));
-    x2 = min(coordinates2{i}(coordinates2{i}(:,3)~=Index(i),1)):0.005:max(coordinates2{i}(coordinates2{i}(:,3)~=Index(i),1));
-    y2 = Line2.a*x2 + Line2.b;
+    Line2fit = fit(coordinates3{i}(coordinates3{i}(:,4)~=Index(i),1),coordinates3{i}(coordinates3{i}(:,4)~=Index(i),2),...
+        ft, 'problem',Line1(i,1),'StartPoint', Line1(i,2));
+    Line2(i,1) = Line1(i,1);
+    Line2(i,2) = coeffvalues(Line2fit);
+    x2 = min(coordinates2{i}(coordinates2{i}(:,4)~=Index(i),1)):0.005:max(coordinates2{i}(coordinates2{i}(:,4)~=Index(i),1));
+    y2 = Line2(i,1)*x2 + Line2(i,2);
     plot(x2, y2 , '-b', 'LineWidth',3);
     cd(linedir);
     print(image2, [num2str(i),'_lines.tif'], '-dtiff', '-r150');
     cd(currdir); 
     close all;
     
-    Distance(i) = abs(Line1(1,2)-Line2.b)/sqrt(Line2.a*Line2.a + 1);
+    Distance(i) = abs(Line1(i,2)-Line2(i,2))/sqrt(Line2(i,1)*Line2(i,1) + 1);
     
 end
 Filenames = 1:1:numel(files);
@@ -152,4 +155,42 @@ headers2 = {'Ring', 'Distance'};
 csvwrite_with_headers(Otput_summary,Distance2, headers2);
 cd(currdir); 
 
+%% Moving, rotating and summing up
+coordinates_rot1 = struct([]);
+coordinates_all = zeros(1,3);
+rotation = zeros(numel(files),1);
+Center = zeros(numel(files),2);
+for i=1:numel(files)
+    rotation(i) = atan2d(Line1(i,1),1);
+    Center(i,1) = mean(coordinates3{i}(coordinates3{i}(:,4)==Index(i),1)) - ...
+        sind(rotation(i))*(Line2(i,2)-Line1(i,2))/sqrt(Line2(i,1)*Line2(i,1) + 1)/2;
+    Center(i,2) = Line2(i,1)*Center(i,1) + (Line1(i,2)+Line2(i,2))/2;
+    
+    R = [cosd(-rotation(i)) -sind(-rotation(i)); sind(-rotation(i)) cosd(-rotation(i))];
+    Center_mat_border = repmat([Center(i,1); Center(i,2)], 1, length(coordinates2{i}))';
+    
+    coordinates_rot1{i} = coordinates2{i}(:,1:2) - Center_mat_border;
+    coordinates_rot1{i} = (R* coordinates_rot1{i}')';
+    coordinates_rot1{i}(:,3) = coordinates2{i}(:,3);
+    
+    coordinates_all = [coordinates_all;coordinates_rot1{i}(:,1:3)];
+end
+coordinates_all(coordinates_all(:,3) ==0,:) =[];
+
+bin_size = 2;
+dist_length = max(max(abs(coordinates_all(:,1:2))));
+binrange = 0 : bin_size : (dist_length*2);
+bincenter=binrange(1:(end-1)) + bin_size/2;
+final_lines = zeros(length(bincenter)+2,length(bincenter)+2);
+for n=1:length(coordinates_all)
+   final_lines(ceil((coordinates_all(n,2)+max(max(abs(coordinates_all(:,1:2)))))/bin_size)+1,...
+       ceil((coordinates_all(n,1)+max(max(abs(coordinates_all(:,1:2)))))/bin_size)+1) = ...
+       final_lines(ceil((coordinates_all(n,2)+max(max(abs(coordinates_all(:,1:2)))))/bin_size)+1,...
+       ceil((coordinates_all(n,1)+max(max(abs(coordinates_all(:,1:2)))))/bin_size)+1) + coordinates_all(n,3);
+end
+final = mtimes(sum(final_lines,2),sum(final_lines,1));
+imshow(final, [0, max(max(final))]);
+[fit_line, gof_line] = fit((1:bin_size:size(final_lines,2)*2)',sum(final_lines,2), 'gauss2');
+plot((1:bin_size:size(final_lines,2)*2)', sum(final_lines,2), 'o',...
+    (1:bin_size:size(final_lines,2)*2)', fit_line((1:bin_size:size(final_lines,2)*2)'),'r');
 cd(pwd);
